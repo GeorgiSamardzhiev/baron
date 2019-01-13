89ed866b035969ba47cb083a4a2342f7697fcd99
GeorgiSamardzhiev <gogitogogito@gmail.com>
2019-01-13 14:47:11 +0200

    removed wrong foldl and foldr implementations

--- higher_order.hpp
+++ higher_order.hpp
@@ -23,24 +23,6 @@ namespace baron {
 		return res;
 	}
 
-	template <typename R, class C, typename Function>
-	R foldl(R base, C list, Function f) {
-		R res = base;
-		for (typename C::iterator it = list.begin(); it != list.end(); ++it) {
-			res = f(res, (*it));
-		}
-		return res;
-	}
-
-	template <typename R, class C, typename Function>
-	R foldr(R base, C list, Function f) {
-		R res = base;
-		for (typename C::reverse_iterator it = list.rbegin(); it != list.rend(); ++it) {
-			res = f(res, (*it));
-		}
-		return res;
-	}
-
 }
 
 #endif // !HIGHER_ORDER_HPP
