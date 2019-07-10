057e23da59404be49e1d777d4c30606bd5d54c91
sashodada <a.shumakov97@gmail.com>
2019-07-10 21:50:27 +0300

    added KMP linear pattern searching algorithm

---
+++ patterns.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <cstring>
+#include <stdio.h>
+
+namespace baron {
+    std::vector<int> prepare_lps(const char *pattern) {
+        int pattern_length = strlen(pattern);
+        std::vector<int> lps(pattern_length);
+        lps[0] = 0;
+
+        int i = 1;
+        int len = 0;
+        while (i < pattern_length) {
+            if (pattern[i] == pattern[len]) {
+                lps[i++] = ++len;
+            } else {
+                if (len) {
+                    len = lps[len - 1];
+                } else {
+                    lps[len] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
+
+    std::vector<int> pattern_search_kmp(const char *text, const char *pattern) {
+        std::vector<int> result;
+
+        auto lps = prepare_lps(pattern);
+        int pattern_length = strlen(pattern);
+        int i = 0;
+        int j = 0;
+        while (i < strlen(text)) {
+            if (j == pattern_length) {
+                result.push_back(i - j);
+                j = lps[j - 1];
+            }
+            if (text[i] == pattern[j]) {
+                ++i;
+                ++j;
+            } else {
+                if (j) {
+                    j = lps[j - 1];
+                } else {
+                    ++i;
+                }
+            }
+        }
+        return result;
+    }
+}
