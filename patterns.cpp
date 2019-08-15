c2765f7c2e1f99ff33fa037088524fd6ffa24ab9
Georgi Samardzhiev <gvsamardzh@uni-sofia.bg>
2019-08-15 18:04:56 +0300

    Empty string check

--- patterns.cpp
+++ patterns.cpp
@@ -1,60 +1,75 @@
-#include <stdio.h>
 #include <cstring>
 #include "patterns.hpp"
 
 namespace baron {
 
-    std::vector<int> prepare_lps(const char *pattern) {
-        size_t pattern_length = strlen(pattern);
-        std::vector<int> lps(pattern_length);
-        lps[0] = 0;
+	std::vector<int> prepare_lps(const char *pattern) {
+		size_t pattern_length = strlen(pattern);
+		std::vector<int> lps(pattern_length);
 
-        int i = 1;
-        int len = 0;
-        while (i < pattern_length) {
-            if (pattern[i] == pattern[len]) {
-                lps[i++] = ++len;
-            } else {
-                if (len) {
-                    len = lps[len - 1];
-                } else {
-                    lps[len] = 0;
-                    i++;
-                }
-            }
-        }
+		if (pattern_length == 0) {
+			return lps;
+		}
 
-        return lps;
-    }
+		lps[0] = 0;
 
-    std::vector<int> pattern_search_kmp(const char *text, const char *pattern) {
-        std::vector<int> result;
+		int i = 1;
+		int len = 0;
+		while (i < pattern_length) {
+			if (pattern[i] == pattern[len]) {
+				lps[i++] = ++len;
+			} else {
+				if (len) {
+					len = lps[len - 1];
+				} else {
+					lps[len] = 0;
+					i++;
+				}
+			}
+		}
 
-        std::vector<int> lps = prepare_lps(pattern);
-        int pattern_length = strlen(pattern);
-        int i = 0;
-        int j = 0;
+		return lps;
+	}
 
-        size_t n = strlen(text);
-        while (i < n) {
-            if (j == pattern_length) {
-                result.push_back(i - j);
-                j = lps[j - 1];
-            }
-            if (text[i] == pattern[j]) {
-                ++i;
-                ++j;
-            } else {
-                if (j) {
-                    j = lps[j - 1];
-                } else {
-                    ++i;
-                }
-            }
-        }
-        if (j == pattern_length) {
-            result.push_back(i - j);
-        }
-        return result;
-    }
+	std::vector<int> pattern_search_kmp(const char *text, const char *pattern) {
+		std::vector<int> result;
+
+		if (text == nullptr || pattern == nullptr) {
+			return result;
+		}
+
+		size_t pattern_length = strlen(pattern);
+
+		if (pattern_length == 0) {
+			return result;
+		}
+
+		std::vector<int> lps = prepare_lps(pattern);
+
+		int i = 0;
+		int j = 0;
+
+		size_t n = strlen(text);
+		while (i < n) {
+			if (j == pattern_length) {
+				result.push_back(i - j);
+				j = lps[j - 1];
+			}
+			if (text[i] == pattern[j]) {
+				++i;
+				++j;
+			} else {
+				if (j) {
+					j = lps[j - 1];
+				} else {
+					++i;
+				}
+			}
+		}
+		if (j == pattern_length) {
+			result.push_back(i - j);
+		}
+		return result;
+	}
+
 }
--- patterns.hpp
+++ patterns.hpp
@@ -1,15 +1,15 @@
-#ifndef BARON_PATTERN_MATCHING_H_
-#define BARON_PATTERN_MATCHING_H_
+#ifndef BARON_PATTERN_MATCHING_HPP
+#define BARON_PATTERN_MATCHING_HPP
 
 #include <vector>
 
 namespace baron {
 
-    // Helper function for KMP algorithm, preprocesses the pattern
-    std::vector<int> prepare_lps(const char *pattern);
+	// Helper function for KMP algorithm, preprocesses the pattern
+	std::vector<int> prepare_lps(const char *pattern);
 
-    // KMP algorithm; linear comlexity O(strlen(text))
-    std::vector<int> pattern_search_kmp(const char *text, const char *pattern);
+	// KMP algorithm; linear comlexity O(strlen(text))
+	std::vector<int> pattern_search_kmp(const char *text, const char *pattern);
 }
 
-#endif // BARON_PATTERN_MATCHING_H_
+#endif // BARON_PATTERN_MATCHING_HPP
